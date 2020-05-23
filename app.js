b893b33754fe75d39e386b91c36a870e2428ca20
JinalAdhyaru <43931022+JinalAdhyaru@users.noreply.github.com>
2020-05-23 17:08:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express= require('express');
 var bodyParser= require('body-parser');
 var mysql = require('mysql');
 var session = require('express-session');
+const passport = require("passport");
 var path = require('path');
 
 var connection = mysql.createConnection({
@@ -19,6 +20,9 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -29,257 +33,207 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-var obj = {};
-var obj3 = {};
-app.post("/auth", function (request, response) {
-    var userid = request.body.usrname;
-    var password = request.body.password;
-
-    //console.log(userid);
-    //console.log(password);
-    if (userid.includes("2018500")) {
-        //console.log("entered student");
-        var sqls = 'SELECT * FROM student WHERE sid = ? AND spaswrd = ?';
-        connection.query(sqls, [userid, password], function (err, result) {
-        if (err) throw err;
-        if(result.length==0)
-        {
-            response.send("student not found");
-        }
-        else{
-        //console.log("found student");
-        connection.query("select sname from student where sid= '"+ userid + "'", function (err,result1) {   
-            if (err) throw err;
-            if(result1.length == 0) {
-                response.send("error");
-            }
-            else {
-               // console.log(result1);
-               response.render('student',{studentname: result[0].sname});
-            }
-
-      });
-      
-        }
-    });
-    }
-    else if (userid.includes("F")) {
-        // console.log("entered faculty");
-        var sqlf = 'SELECT * FROM faculty WHERE fid = ? AND fpaswrd = ?';
-    connection.query(sqlf, [userid, password], function (err, result) {
-        if (err) throw err;
-        if(result.length == 0)
-        {
-            response.send("faculty not found");
-        }
-        else{
-            connection.query("select fname from faculty where fid = '"+ userid +"'", function(err,result2) {
-                if(err) throw err;
-                if(result2.length == 0)
-                {
-                    response.send("no such faculty");
-                }
-                else {
-                    //console.log(result2);
-                    connection.query("SELECT course.cname FROM course natural join faculty where fid = '"+ userid + "'", function (err, result1) {
-                        if (err) throw err;
-                        //console.log(f);  
-                        if(result1.length==0)
-                        {
-                        response.send("no course");
-                        }           
-                      else {
-                          //console.log(result1);
-                          obj.facname = result2;
-                          obj.users = result1;
-                          response.render('home',obj);                
-                      }
-                    });            
-                }
-            });          
-        }        
-    });
-    }
-    else {
-        response.send("incorrect values");
-    }
-    
+app.get("/faculty", function(req,res){
+    res.render("faculty");
 });
+
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/errors",function(req,res){
+    res.redirect("/");
+});
+
+
+var obj = {};
+app.post("/login", function (request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username.includes("2018500")) {
+        connection.query("select * from student where sid= '"+ username +"' and spaswrd = '"+ password+"'", function(err,result) {
+            if (err) throw err;
+            if(result.length==0) { response.render("errors",{message: "Student not found."}); }
+            else{
+                connection.query("select sname from student where sid= '"+ username + "'", function (err,result1) {   
+                    if (err) throw err;
+                    if(result1.length == 0) { response.render("errors",{message: "Student not found."}); }
+                    else {
+                        response.render('student',{studentname: result[0].sname});
+                    }
+                });      
+            }
+        });
+    }
+    else if (username.includes("F")) {
+        connection.query("select * from faculty where fid= '"+ username +"' and fpaswrd = '"+ password+"'", function(err,result) {
+            if (err) throw err;
+            if(result.length == 0) {  response.render("errors",{message: "Faculty not found."}); }
+            else {
+                connection.query("select fname from faculty where fid = '"+ username +"'", function(err,result2) {
+                    if(err) throw err;
+                    if(result2.length == 0) { response.render("errors",{message: "Faculty not found."}); }
+                    else {
+                        connection.query("SELECT course.cname FROM course natural join faculty where fid = '"+ username + "'", function (err, result1) {
+                            if (err) throw err;
+                            if(result1.length==0) { response.render("errors",{message: "No course found."}); }           
+                            else {
+                                obj.facname = result2;
+                                obj.courses = result1;
+                                response.render('faculty',obj);                
+                            }
+                        });            
+                    }
+                });          
+            }        
+        });
+    }
+    else { response.render("errors",{message: "The values you entered are incorrect.Please try again."});  }
+});
+
+
 
 var obj2={};
-app.post('/mark',function(request,response){
-    var sub = request.body.addattendance;
-    console.log(sub);
-    var sqlq='select student.sid,student.sname,course.cid,course.cname,faculty.fid,faculty.fname from course natural join faculty,student where cname=?';
-    connection.query(sqlq,[sub],function(err,result){
+app.post('/addattendance',function(request,response){
+    var subject = request.body.choosesubject;
+    connection.query("select student.sid,student.sname,course.cid,course.cname,faculty.fid,faculty.fname from course natural join faculty,student where cname='"+subject+"'",function(err,result){
         if(err) throw err;
-        if(result.length==0){
-            response.send("no record found");
-        }
+        if(result.length==0){ response.render("Finalpage",{message: "No such record exists."}); }
         else{
-            console.log(result);
-            obj2 = {attend: result};
-            console.log(obj2);
-            response.render('attend',obj2);
-
+           obj2 = {data: result};
+           response.render('attendancepage',obj2);
         }
     });
 });
 
-app.post('/lastpage', function (req, res) {    
+app.post('/addingattendance', function (req, res) {    
     const d = new Date();
-    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
-    const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d)
-    const da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d)
-
+    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+    const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+    const da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);    
     var dayy = `${da}_${mo}_${ye}`;
-    var attended = req.body.selectpick;
+    var attended = req.body.markattendance;
     var sids = req.body.sid;
     var cid = req.body.cid.toLowerCase();
     var cn = cid + "_attendance";
-    // var fid = request.body.fid;
-    //console.log(sids.length);
-    //console.log(sids);
-    //console.log(attended);
-    //console.log(cn);
-    //console.log(dayy);
-    var alsql = 'alter table '+ cn + ' add column ' + dayy + ' varchar(5)';
-    connection.query(alsql,function(err,result){
+    connection.query("alter table " + cn + " add column " + dayy +" varchar(1)",function(err,result) {
        if(err) throw err;
-       console.log("altered successfully");
        for(var i=0;i<sids.length;i++)
        {
-           var upsql='update ' + cn + ' set ' + dayy + '= ? where sid = ?';
-           connection.query(upsql,[attended[i],sids[i]],function(err,result){
-            if(err) throw err;
-            console.log("recorded successfully");
-            
+           connection.query("update "+ cn + " set " + dayy + " = '" + attended[i] +"' where sid='"+ sids[i]+"'",function(err,result) {
+            if(err) throw err;      
            });
        }
-       //res.render('home');
-        });
+       res.render("Finalpage",{message: "The attendance has been successfully recorded and stored in the database."});
     });
-
-
-app.post('/fview',function(request,response){
+});
+    
+app.post('/viewattendance',function(request,response){
     var arrclname = [];
     var arr = [];
-    var crs = request.body.viewatt;
-    //console.log("ur subject is taken" + crs);
-    var viewquery = "select cid from course where cname = '"+crs+"'";
-    connection.query(viewquery,[crs],function(err,result){
+    var arrsname = [];
+    var crs = request.body.choosesubject;
+    connection.query("select cid from course where cname = '" + crs + "'",function(err,result){
         if (err) throw err;
-        if (result.length == 0) {
-            response.send("no attendance found");
-        }
+        if (result.length == 0) { response.render("Finalpage",{message: "No such record exists."}); }
         else {
-            var arr = [];
-            var tbname = result[0].cid+"_attendance";
-            connection.query("show columns from "+ tbname.toLowerCase(), function(err,columnname){
+            connection.query("select cid,cname,fid,fname from course natural join faculty where cid='"+ result[0].cid +"'", function(err,data) {
                 if(err) throw err;
-                if(columnname.length == 0) { 
-                    console.log("Table does not exist");
-                }
-                else {                    
-                    for(var j=0;j<columnname.length;j++){
-                        arrclname.push(columnname[j].Field);
-                    }
-                    console.log("arrclname : " + arrclname);
-                    for(var k=0;k<arrclname.length;k++) {
-                        connection.query("select "+ arrclname[k] +" from "+ tbname.toLowerCase(), function(err,result1) {
-                            if(err) throw err;
-                            if(result.length == 0) {
-                                console.log("Column does not exist");
-                            }
-                            else {      
-                               console.log(result1);
-                               var arr = [];
-                               for(var i=0;i<result1.length;i++) {
-                                    arr.push(result1[i].arrclname);
-                                    console.log(arr);
-                               }
-                               console.log("Arr " + arr);
-                            }
-                        });
-                    }
-                    //console.log("ARR "+ arr);
-                    response.render("fview",{cols: arrclname,fviews: arr})
-                   // console.log(arr);
-                   // console.log(arrclname);
-                //    connection.query(" select * from " + tbname.toLowerCase() , function(err,result1) {
-                //         if (err) throw err;
-                //         if(result1.length == 0) { 
-                //             console.log("table does not exist")
-                //         }
-                //         else {
-                //             const arrsid = [];
-                //             console.log(result1);
-                //             for(var i=0;i<result1.length;i++)
-                //             {
-                //                 arrsid.push(result1[i].sid);
-                //             }
-                //             //console.log(result1.length*arrclname.length);
-                //             response.render("fview",{cols: arrclname,fviews: arrsid})
-                //         }
-                //     });
+                else if(data.length == 0) {console.log("Incorrect entry");}
+                else {
+                    arr.push(data[0].cid);
+                    arr.push(data[0].cname);
+                    arr.push(data[0].fid);
+                    arr.push(data[0].fname);
                 }
             });
+            connection.query("select sname from student",function(err,snames) {
+                if(err) throw err;
+                else if(snames.length == 0) { response.render("Finalpage",{message: "The requested data does not exists."});}
+                else {
+                    for(var i=0;i<snames.length;i++) {
+                        arrsname.push(snames[i].sname);
+                    }
+                }
+            });
+            var tbname = result[0].cid+"_attendance";                           
+            connection.query("show columns from "+ tbname, function(err,columns) {
+                if(err) throw err;
+                else if(columns.length == 0) { response.render("Finalpage",{message: "The requested data does not exists."});  }
+                else {
+                    for(var i=0;i<columns.length;i++) {
+                        arrclname.push(columns[i].Field);
+                    }
+                    connection.query("select * from " + tbname , function(err,results) {
+                        if(err) throw err;
+                        else if(results.length == 0) {  response.render("Finalpage",{message: "The requested data does not exists."}); } 
+                        else { response.render("facultyattendanceview" ,{studentnm: arrsname,data: arr,cols: arrclname,info: results});   }
+                    });
+                }       
+            });                   
         }
     });
 });
 
-
-app.post('/attendance', function (request, response) {
+app.post('/student', function (request, response) {
     var arrclname = []; 
-    var arr = [];
+    var arr=[];
     var sid;
-    console.log(request.body.selectpick);
-    var crname = request.body.selectpick;
+    var crname = request.body.choosesubject;
     var stdn = request.body.studentname;
-    console.log(crname);
-    console.log(stdn);
     connection.query("select sid from student where sname = '" + stdn + "'", function(err,sids) {
         if(err) throw err;
         else if(sids.length == 0) {
-            console.log("Student Id not found");
+            response.render("Finalpage",{message: "The requested data does not exists."});
         }
         else {
-            console.log(sids);
             sid = sids[0].sid;
-            console.log(sid);
         }
-    });
+    });        
     connection.query("select cid from course where cname = '" + crname + "'", function(err, result) {
         if (err) throw err;
-        if(result.length == 0) { console.log("Course does not exist")}
+        if(result.length == 0) {response.render("Finalpage",{message: "The requested data does not exist."}); }
         else {
-            console.log(result);
+            connection.query("select cid,cname,fid,fname from course natural join faculty where cid='"+ result[0].cid +"'", function(err,data) {
+                if(err) throw err;
+                else if(data.length == 0) {response.render("Finalpage",{message: "The requested data does not exists."});}
+                else {
+                    arr.push(data[0].cid);
+                    arr.push(data[0].cname);
+                    arr.push(data[0].fid);
+                    arr.push(data[0].fname);
+                }
+            });
+            connection.query("select sname from student where sid='"+ sid +"'", function(err,nameofstudent) {
+                if(err) throw err;
+                else if(nameofstudent.length == 0) {response.render("Finalpage",{message: "The requested data does not exists."});}
+                else {
+                    arr.push(nameofstudent[0].sname);
+                }
+            });
             var x = result[0].cid;
             var tbname = x.toLowerCase() + "_attendance";
-            console.log(stdn);
-            console.log(tbname);
             connection.query("show columns from "+ tbname, function(err,columns) {
-             if(err) throw err;
-             else if(columns.length == 0) { console.log("No columns found");  }
-             else {
-                 console.log(columns);
-                 for(var i=0;i<columns.length;i++) {
-                     arrclname.push(columns[i].Field);
-                 }
-                 console.log(arrclname);
-                 connection.query("select * from " + tbname + " where sid = '" + sid + "'", function(err,results) {
-                    if(err) throw err;
-                    else if(results.length == 0) { console.log("No entry found");} 
-                    else {
-                        console.log(results);
+                if(err) throw err;
+                else if(columns.length == 0) {response.render("Finalpage",{message: "The requested data does not exists."});  }
+                else {
+                    for(var i=0;i<columns.length;i++) {
+                        arrclname.push(columns[i].Field);
                     }
-                    response.render("studentattendance" ,{cols: arrclname,fviews: results});
-                 });
-             }       
+                    connection.query("select * from " + tbname + " where sid = '" + sid + "'", function(err,results) {
+                        if(err) throw err;
+                        else if(results.length == 0) { response.render("Finalpage",{message: "The requested data does not exists."}); } 
+                        else {                       
+                            response.render("studentattendance" ,{data: arr,cols: arrclname,fviews: results});
+                        }
+                    });
+                }       
             });
         }
-        });   
-    });
+    });   
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
